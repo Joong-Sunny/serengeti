7b69b83ac2bc7ca34d227b0eb3e39bc24a0ed12a
sunny <dance@kakao.com>
2023-09-24 22:19:03 +0900

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Serengeti Project, Lets go</h1>
+      <h1>Serengeti Project, Lets go </h1>
 
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>Jinha, Sunny, Okorion {count}</button>
